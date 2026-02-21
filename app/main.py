ad1920dc4362596f20f9ee9472b0b761430b036f
Miron Razvan <mrsrazvan2004@gmail.com>
2026-02-21 17:50:37 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ import sys
 
 
 def main():
+    # begin repl with unprivileged user tag
     sys.stdout.write("$ ")
-
+    # read user input
+    cmd = input()
+    print(f"{cmd}: command not found")
+    
 
 if __name__ == "__main__":
     main()
